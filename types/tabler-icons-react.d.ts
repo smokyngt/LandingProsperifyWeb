f4546bf41b809105981d47ba31dc31ce29c6ce67
smokyngt <brikielyes9@gmail.com>
2025-12-28 21:01:37 +0100

    Rebuild Next.js with fresh chunks

--- types/tabler-icons-react.d.ts
+++ types/tabler-icons-react.d.ts
@@ -1,9 +1,11 @@
-import * as React from "react";
-
 declare module "@tabler/icons-react" {
-  export interface IconProps extends React.SVGProps<SVGSVGElement> {}
+  import * as React from "react";
+
+  export interface IconProps extends React.SVGProps<SVGSVGElement> {
+    size?: number | string;
+    stroke?: number | string;
+  }
 
   export const IconMenu2: React.FC<IconProps>;
   export const IconX: React.FC<IconProps>;
-  // Add other icons here if you start using them
 }
